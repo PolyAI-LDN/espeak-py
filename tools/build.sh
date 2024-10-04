c875a1fc5c46759f185c0b223cc5377ba41beaca
Paul Annetts <5033948+paulannetts@users.noreply.github.com>
2024-10-04 18:13:33 +0100

    Fix up the fix up for autotools

--- tools/build.sh
+++ tools/build.sh
@@ -6,7 +6,8 @@ PARENT_DIR=$(dirname $SCRIPT_DIR)
 echo $PARENT_DIR
 cd $PARENT_DIR/espeak-ng
 rm CHANGELOG.md
-echo "Hi" > ChangeLog.md
+echo "Changelog dummy" > ChangeLog.md
+echo "No news" > NEWS
 ls
 ./autogen.sh
 ./configure --without-klatt --without-speechplayer --without-mbrola --without-sonic --without-async
