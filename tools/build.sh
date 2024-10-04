7f9d31ebd01b697749c09940796e2ff9a365c8f7
Paul Annetts <5033948+paulannetts@users.noreply.github.com>
2024-10-04 17:36:05 +0100

    debug 1

--- tools/build.sh
+++ tools/build.sh
@@ -3,7 +3,9 @@ set -e
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 PARENT_DIR=$(dirname $SCRIPT_DIR)
 
+echo $PARENT_DIR
 cd $PARENT_DIR/espeak-ng
+ls
 touch ChangeLog.md
 ./autogen.sh
 ./configure --without-klatt --without-speechplayer --without-mbrola --without-sonic --without-async
