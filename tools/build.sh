ba9ded49272d22b7d6d6a159023e1fa01f59bd10
Paul Annetts <5033948+paulannetts@users.noreply.github.com>
2024-10-04 17:34:27 +0100

    GHA try 1

--- tools/build.sh
+++ tools/build.sh
@@ -3,12 +3,8 @@ set -e
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 PARENT_DIR=$(dirname $SCRIPT_DIR)
 
-
 cd $PARENT_DIR/espeak-ng
 touch ChangeLog.md
 ./autogen.sh
 ./configure --without-klatt --without-speechplayer --without-mbrola --without-sonic --without-async
 make
-
-cd $PARENT_DIR
-RUSTFLAGS='-L espeak-ng/src/.libs' maturin build --release
