d8aa2bbb2a29d2ed77a4f9ea6827566d5ba3cefc
Paul Annetts <5033948+paulannetts@users.noreply.github.com>
2024-10-07 13:49:57 +0100

    Try and fix ubuntu by C gcc flag

--- tools/build.sh
+++ tools/build.sh
@@ -13,4 +13,8 @@ fi
 ls
 ./autogen.sh
 ./configure --without-klatt --without-pcaudiolib --without-mbrola --without-sonic --without-async
+
+# Fix error on Ubuntu 22.04+
+# See https://stackoverflow.com/questions/76060903/gcc-multiple-definition-of-error-on-ubuntu-22-04-after-updating-from-ubuntu-2
+export CFLAGS="-fcommon"
 make
